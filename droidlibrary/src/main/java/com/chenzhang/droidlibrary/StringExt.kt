80ffbc3b72109df61c57ddedbed0586b9c7d29c6
Wenbin <liuwenbin168@126.com>
2021-08-09 23:18:22 +0800

    test again 2

--- droidlibrary/src/main/java/com/chenzhang/droidlibrary/StringExt.kt
+++ droidlibrary/src/main/java/com/chenzhang/droidlibrary/StringExt.kt
@@ -2,4 +2,4 @@ package com.chenzhang.droidlibrary
 
 fun String.getMessageFromTransitiveDependency(): String = "\"$this\" from Transitive Dependency"
 
-fun String.x() = "xxx"
+fun String.x() = "xxxx"
