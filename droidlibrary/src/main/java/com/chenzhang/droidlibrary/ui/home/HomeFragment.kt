9dee4c8dc90688ea626160597b1a918a7a2d0d78
Wenbin <liuwenbin168@126.com>
2021-08-18 16:36:48 +0800

    test minify

---
+++ droidlibrary/src/main/java/com/chenzhang/droidlibrary/ui/home/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.chenzhang.droidlibrary.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.chenzhang.droidlibrary.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+        View root = inflater . inflate (R.layout.fragment_home, container, false);
+        val textView: TextView = root.findViewById<TextView>(R.id.text_home)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+}
